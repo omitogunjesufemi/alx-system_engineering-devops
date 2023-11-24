a1d9a0f6b06e688ee7dd03e59acc0d5964388cee
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-11-24 22:48:05 +0100

    Added python3-pip package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,17 @@
 # Installing flask from pip3 using Puppet
 
+package { 'python3-pip':
+  ensure   => 'installed'
+}
+
 package { 'flask':
   ensure   => 'installed',
   provider => 'pip3',
+  require  => Package['python3-pip']
 }
 
 package { 'werkzeug':
   ensure   => 'installed',
   provider => 'pip3',
+  require  => Package['python3-pip']
 }
