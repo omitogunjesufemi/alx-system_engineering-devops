9f0a7988b10055e24297889a48729908100a5889
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-11-27 03:58:34 +0100

    Using Puppet to make changes to configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,18 @@
 # Puppet to make changes to ssh configuration file
 
-$config_str = "Host *
-IdentityFile ~/.ssh/school
-PasswordAuthentication no
-"
+file_line { 'Turn off passwd auth':
+  ensure             => 'present',
+  path               => '/home/jesufemi/Documents/alx_projects/alx-system_engineering-devops/0x0B-ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => 'PasswordAuthentication',
+  append_on_no_match => true
+}
 
-file { 'ssh_config':
-  ensure  => 'present',
-  path    => '/etc/ssh/ssh_config_',
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-  content => $config_str
+file_line { 'Declare identity file':
+  ensure             => 'present',
+  path               => '/home/jesufemi/Documents/alx_projects/alx-system_engineering-devops/0x0B-ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => 'IdentityFile',
+  multiple           => true,
+  append_on_no_match => true
 }
