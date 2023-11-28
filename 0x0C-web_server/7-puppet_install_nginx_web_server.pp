f682a27de81cd3a933c1565415343f5b91058674
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-11-28 15:05:57 +0100

    Adding puppet manifest for configuration

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,12 +7,12 @@ package { 'nginx':
 }
 
 file { 'index.html':
-  path => "/var/www/html/index.html",
+  path    => '/var/www/html/index.html',
   content => "Hello World!\n",
   require => Package['nginx']
 }
 
 exec { 'redirection':
-  path => '/usr/bin/',
+  path    => '/usr/bin/',
   command => "sudo sed -i 's|^[^#].*server_name.*;|\\tserver_name _;\\n\\trewrite ^/redirect_me$ http://x.com permanent;\\n\\trewrite ^/redirect_me/$ http://x.com permanent;|' /etc/nginx/sites-enabled/default;",
 }
