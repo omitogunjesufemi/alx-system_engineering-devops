d6f18a0be830209711c507721b0603508ae03bdd
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2023-12-05 02:54:17 +0100

    Puppet manifest for task 0

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,1 +1,24 @@
 # Puppet automation to configure web-02 to be identical to web-01
+include stdlib
+
+package { 'nginx':
+  ensure => 'installed'
+}
+
+$str="        server_name _;
+        add_header X-Served-By \"${hostname}\";
+
+"
+
+file_line { 'redirection_server':
+  path  => '/etc/nginx/sites-enabled/default',
+  line  => $str,
+  match => '^\s*server_name _;.*$',
+  after => '^\s*server_name _;.*$'
+}
+
+exec { 'restart nginx':
+  path    => '/usr/bin',
+  command => 'sudo service nginx restart',
+  require => [Package['nginx'], File_line['redirection_server']]
+}
