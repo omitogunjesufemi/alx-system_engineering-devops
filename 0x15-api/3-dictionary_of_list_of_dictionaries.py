1d148569a8310644c243378926fb7cb840ae9ef2
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-01-02 23:15:39 +0100

    Export all employees data into JSON format

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Extend task 0 to export data into JSON format
+- Records all tasks from all employees
+- Format must be:
+  { "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE",
+               "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME",
+               "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ],
+   "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE",
+              "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME",
+              "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ]}
+- File name must be: todo_all_employees.json
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    usr_url = f"https://jsonplaceholder.typicode.com/users"
+    usr_response = requests.get(usr_url)
+    users = usr_response.json()
+
+    output_dict = {}
+    for user in users:
+        usr_id = user.get("id")
+        url = f"https://jsonplaceholder.typicode.com/todos/?userId={usr_id}"
+        todo_response = requests.get(url)
+        todos = todo_response.json()
+        task_list = []
+        for todo in todos:
+            task_dict = {}
+            task_dict["task"] = todo.get("title")
+            task_dict["completed"] = todo.get("completed")
+            task_dict["username"] = user.get("username")
+            task_list.append(task_dict)
+
+        output_dict[str(user.get("id"))] = task_list
+
+    with open("todo_all_employees.json", "w", encoding="UTF-8") as json_file:
+        json_file.write(json.dumps(output_dict))
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -20,3 +20,4 @@ TASK | DESCRIPTION
 `0-gather_data_from_an_API.py` | Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress
 `1-export_to_CSV.py` | Extends task 0 to export data in the CSV format
 `2-export_to_JSON.py` | Extend task 0 to export data in the JSON format
+`3-dictionary_of_list_of_dictionaries.py` | Extend task 0 to export data for all employees into JSON format
