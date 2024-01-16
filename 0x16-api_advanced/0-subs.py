fa4d810213b1a1540d9d1fd7d02cacdece99c99e
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-01-16 13:51:43 +0100

    Added try/except block to check of requests works

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,11 +17,17 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
     }
-    response = requests.get(url_reddit, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        reddit_response = response.json()
-        reddit_data = reddit_response.get("data")
-        return reddit_data.get("subscribers")
-    else:
+    try:
+        response = requests.get(url_reddit,
+                                headers=headers,
+                                allow_redirects=False)
+
+        if response.status_code == 200:
+            reddit_response = response.json()
+            reddit_data = reddit_response.get("data")
+            return reddit_data.get("subscribers")
+        else:
+            return 0
+    except requests.exceptions.RequestException:
         return 0
