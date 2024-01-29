f54502e264ab4e716ae3b8adca17b76fc31ab09b
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-01-30 00:44:15 +0100

    Set file limit to 30000

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # increased the max number of files opened by nginx
 
 exec {'nofiles':
-  command => "sudo sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g' /etc/default/nginx",
+  command => "sudo sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 30000\"/g' /etc/default/nginx",
   path    => '/usr/local/bin/:/bin/',
 }
